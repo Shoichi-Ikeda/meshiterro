b83851fe8a240e2318800f5f9d47e119d209ce49
Shoichi-Ikeda <shoichann@gmail.com>
2021-02-11 22:09:54 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ea5dbaf53c9218d9eb94d4f9c8e0af4b899e11a15e63f8a50937b128fd32684383db0624f83e6eccce34e58e2dc2a366d932c133951173e136cd13308ea3ed9a'
