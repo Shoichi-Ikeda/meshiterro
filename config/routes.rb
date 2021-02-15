21d8a29a98d718bbab8ee7b63e872399a9eff43d
Shoichi-Ikeda <shoichann@gmail.com>
2021-02-15 12:42:22 +0000

    [Finish] 4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :user
   root 'post_images#index'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
+end
